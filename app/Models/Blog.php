9897ba3b91ffe6454f47cd4ffdac42868401909d
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-04-23 17:37:26 +0700

    update fillable di model Blog
    - antisipasi mass input vulnerability
    - data yg boleh di isi secara mass

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    //
+    //fillable
+    protected $fillable = ['blog_title', 'blog_content', 'blog_images', 'blog_author', 'blog_status'];
 }

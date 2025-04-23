4ae16e208a17d7406d30f80a203a8d165bc7a904
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-04-23 12:17:59 +0700

    setup alpineJS
    npm install alpinejs
    
    lanjut tambahkan code di app.js
    add alpinecode di blade
    
    yarn run dev

---
+++ resources/views/test.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test Alpine</title>
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
+</head>
+<body>
+    <h1>Test AlpineJS</h1>
+    <div x-data="{ count: 0 }">
+        <button x-on:click="count++">Tambah</button>
+        <p>x-data count: <span x-text="count"></span></p>
+    </div>
+
+</body>
+</html>
